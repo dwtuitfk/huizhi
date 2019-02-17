d42368e8f1df7975044347e13d51cc4191d52bc0
yanchao <763470346@qq.com>
2019-02-17 12:17:03 +0800

    邮箱服务实现类

---
+++ src/main/java/com/huizhi/oa/service/impl/EmailinfoServiceImpl.java
@@ -0,0 +1,42 @@
+package com.huizhi.oa.service.impl;
+
+import com.huizhi.oa.entity.Emailinfo;
+import com.huizhi.oa.service.EmailinfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailinfoServiceImpl<service> implements EmailinfoService {
+    @Autowired
+    private EmailinfoService emailinfoService;
+
+    @Override
+    public int deleteByPrimaryKey(String eId) {
+        return 0;
+    }
+
+    @Override
+    public int insert(Emailinfo record) {
+        return 0;
+    }
+
+    @Override
+    public int insertSelective(Emailinfo record) {
+        return 0;
+    }
+
+    @Override
+    public Emailinfo selectByPrimaryKey(String eId) {
+        return null;
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(Emailinfo record) {
+        return 0;
+    }
+
+    @Override
+    public int updateByPrimaryKey(Emailinfo record) {
+        return 0;
+    }
+}
